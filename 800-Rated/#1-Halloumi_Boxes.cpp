e02bfbc146505feac8b76082cdd45d6d23af0a2c
vyakhyarastogi <vyakhyarastogi975@gmail.com>
2025-06-23 17:54:20 +0530

    800

---
+++ 800-Rated/#1-Halloumi_Boxes.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, k;
+        cin >> n >> k;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        vector<long long> copy_a = a;
+        sort(copy_a.begin(), copy_a.end());
+
+        if (copy_a == a || k > 1)
+        {
+            cout << "YES" << "\n";
+        }
+        else
+        {
+            cout << "NO" << "\n";
+        }
+    }
+}

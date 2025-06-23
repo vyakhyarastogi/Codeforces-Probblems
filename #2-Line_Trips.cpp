8691d41df239322dfaac6071f168d478c2b4939b
vyakhyarastogi <vyakhyarastogi975@gmail.com>
2025-06-23 18:38:25 +0530

    Q2

---
+++ #2-Line_Trips.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, x;
+        cin >> n >> x;
+        vector<long long> points_arr;
+        points_arr.push_back(0);
+        for (int i = 0; i < n; i++)
+        {
+            long long point;
+            cin >> point;
+            points_arr.push_back(point);
+        }
+        points_arr.push_back(x);
+        n = points_arr.size();
+
+        long long max_distance_between_points = INT_MIN;
+        for (int i = 1; i < n; i++)
+        {
+            if (i == n - 1)
+            {
+                max_distance_between_points = max(max_distance_between_points, 2 * (points_arr[i] - points_arr[i - 1]));
+            }
+            else
+            {
+                max_distance_between_points = max(max_distance_between_points, points_arr[i] - points_arr[i - 1]);
+            }
+        }
+        cout << max_distance_between_points << "\n";
+    }
+}
